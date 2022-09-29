cba707a061b4a102f394bd82941678127e4daeed
cjnour <charbel.nour9@gmail.com>
2022-09-29 19:58:47 -0400

    Nav controls with colours

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React from "react";
 import "./App.css";
+import "./styles.css";
 
 const App = () => {
   return (
-    <div id="container">
-      <div id="right">
-        <h4>home</h4>
-        <h4>work</h4>
-        <h4>experience</h4>
-        <h4>contact</h4>
-      </div>
+    <div>
+      <h4
+        className="home"
+        onClick={() => {
+          console.log("home");
+        }}
+      >
+        home
+      </h4>
+      <h4
+        className="work"
+        onClick={() => {
+          console.log("work");
+        }}
+      >
+        work
+      </h4>
+      <h4
+        className="exp"
+        onClick={() => {
+          console.log("exp");
+        }}
+      >
+        experience
+      </h4>
+      <h4
+        className="con"
+        onClick={() => {
+          console.log("con");
+        }}
+      >
+        contact
+      </h4>
     </div>
   );
 };

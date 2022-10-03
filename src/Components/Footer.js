798cb787e09203b68c51a006f31fad86ab2106f4
cjnour <charbel.nour9@gmail.com>
2022-10-03 18:51:26 -0400

    Updated work page w/ Moneris.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,27 +4,17 @@ import LilMouse from "./LilMouse";
 
 function Footer() {
   return (
-    <div>
-      <div style={{ position: "fixed", bottom: "-0.1rem", left: "1rem" }}>
-        <p>
-          <div style={{ display: "flex", flexDirection: "row" }}>
-            <p style={{ color: "red" }}>a</p>
-            <p style={{ color: "blue" }}>website</p>
-            <p style={{ color: "rgb(0, 202, 0)" }}>by</p>
-            <a
-              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-              target="_blank"
-            >
-              <img
-                src={cjnour_logo}
-                alt="chese"
-                style={{ width: "6.25rem", height: "2.98rem" }}
-              />
-            </a>
-          </div>
-        </p>
-      </div>
-      <LilMouse />
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <p style={{ color: "red" }}>a</p>
+      <p style={{ color: "blue" }}>website</p>
+      <p style={{ color: "rgb(0, 202, 0)" }}>by</p>
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+        <img
+          src={cjnour_logo}
+          alt="chese"
+          style={{ width: "6.25rem", height: "2.98rem" }}
+        />
+      </a>
     </div>
   );
 }

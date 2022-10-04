1c952699c0b1b004ba40290169c4f64ba73d4000
cjnour <charbel.nour9@gmail.com>
2022-10-04 10:28:10 -0400

    Updated Redux settings.

--- src/Redux/Store/ConfigureStore.js
+++ src/Redux/Store/ConfigureStore.js
@@ -13,6 +13,8 @@ export const store = createStore(
   rootReducer(history),
   compose(
     applyMiddleware(routerMiddleware(history), thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
   )
 );
